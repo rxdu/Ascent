b64704bf943abb2032a723cd27d383203341caba
Ruixiang Du <ruixiang.du@gmail.com>
2018-03-20 16:19:47 -0400

    re-organized repository structure

---
+++ build/examples/airy/CMakeFiles/airy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/airy.dir/airy.cpp.o"
+  "airy.pdb"
+  "airy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/airy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/examples/examples/modular-spring-damper/CMakeFiles/modular-spring-damper.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rdu/Workspace/Ascent/examples/examples/modular-spring-damper/Main.cpp" "/home/rdu/Workspace/Ascent/build/examples/examples/modular-spring-damper/CMakeFiles/modular-spring-damper.dir/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ascent"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
